98e9e12dc8374ab96987d55366d1594fa136ee79
Chenzhumei <zcm_1018@163.com>
2021-07-18 21:08:25 +0800

    完成轮播图功能

--- src/pages/Home/Component/HomeCarousel/HomeCarousel.jsx
+++ src/pages/Home/Component/HomeCarousel/HomeCarousel.jsx
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Carousel } from 'antd';
+import {LeftOutlined, RightOutlined} from '@ant-design/icons'
 import './HomeCarousel.less'
 import {reqBanner} from './../../../../api/homeApi'
 
 export default class HomeCarousel extends Component {
     state = {
         banners:[] 
+    }
+    carouselRef = React.createRef()
+    next = () => {
+     this.carouselRef.current.next()
+    }
+    prev = () => {
+        this.carouselRef.current.prev()
     }
     componentDidMount() {
         reqBanner().then(res => {
@@ -16,19 +24,23 @@ export default class HomeCarousel extends Component {
     render() {
         const {banners} = this.state;
         return (
-                banners.length?
-                <Carousel style={{width:730}} autoplay>
-                    {
-                        banners.map(item => (
-                            <div key={item.targetId}>
-                                <a href={item.url} target="_blank" className="banner-item">
-                                    <img src={item.imageUrl} />
-                                </a>
-                            </div>
-                        ))
-                    }
-                </Carousel>
-                : <div>没有数据</div>
+               <div className="carousel">
+                    <Carousel style={{width:730}} autoplay ref={this.carouselRef}>
+                        {
+                            banners.map(item => (
+                                <div key={item.targetId}>
+                                    <a href={item.url} target="_blank" className="banner-item">
+                                        <img src={item.imageUrl} />
+                                    </a>
+                                </div>
+                            ))
+                        }
+                   </Carousel>
+                   <LeftOutlined className="left-icon" onClick={this.prev}/>
+                   <RightOutlined  className="right-icon" onClick={this.next}/>
+               </div>
+               
+              
         )
     }
 }
